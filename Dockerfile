98b050ff75ffce16d3d466d80c7a3aee428d542f
Jacques Supcik <jacques@supcik.net>
2024-12-26 13:40:02 +0100

    Refactor Dockerfile and entrypoint script to use Poetry for dependency management and remove requirements.txt

--- Dockerfile
+++ Dockerfile
@@ -2,15 +2,17 @@ FROM python:3.13-slim
 
 RUN apt-get update && apt-get install -y \
     gcc \
+    pipx \
     python3-dev \
     && rm -rf /var/lib/apt/lists/*
 
+ENV PATH="$PATH:/root/.local/bin"
+
 WORKDIR /app
+COPY app pyproject.toml poetry.lock ./
 
-COPY requirements.txt .
-RUN pip install --no-cache-dir -r requirements.txt
-
-COPY . .
+RUN pipx install poetry
+RUN poetry install
 
 # Use an entrypoint script to allow for variable substitution
 COPY entrypoint.sh /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ PORT="${PORT:-5000}"
 
 if [ "$DEBUG" = "True" ] || [ "$DEBUG" = "1" ] || [ "$DEBUG" = "true" ]; then
     echo "Running in DEBUG mode"
-    exec python -c "from app import app, socketio; socketio.run(app, host='0.0.0.0', port=$PORT, debug=True)"
+    exec poetry run python -c "from app import app, socketio; socketio.run(app, host='0.0.0.0', port=$PORT, debug=True)"
 else
     echo "Running in PRODUCTION mode"
-    exec gunicorn --worker-class gevent -w 1 -b "0.0.0.0:$PORT" app:app
+    exec poetry run gunicorn --worker-class gevent -w 1 -b "0.0.0.0:$PORT" app:app
 fi
